f55a693a6a08dc8ed523b62da11e19f74cf361fe
harishcdw <125392411+harishcdw@users.noreply.github.com>
2023-04-04 18:06:05 +0530

    assignment3

--- assignment3/index.js
+++ assignment3/index.js
@@ -1,31 +1,59 @@
 
-const fs=require('fs');
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const {randomSplice}=require("random-splice");
+
 // reading json file
+let colorPaletteFile = "./color_palette.json";
 
+// function for reading a file
+async function readingFile(filePath) {
+    try{
+        let data = await fs.promises.readFile(filePath, "utf-8");
+        data = JSON.parse(data);
+        if(data.length>=5){
+            return data;
+        }
+        return "Data not sufficient";
+    }
+    catch(err){
+        return err;
+    }  
+}
+function writeFile(filePath,data){
+    fs.writeFileSync(filePath, JSON.stringify(data));
+}
 
-http.createServer((req,res)=>{ 
-    if(req.url != "/favicon.ico") {
-        let colorPaletteFile = "./color_palette.json";
-        fs.readFile(colorPaletteFile, "utf-8",(err,data)=>{
-            data=JSON.parse(data);
-            console.log( data);
-            const randomSet = new Set();
-            let colorPalettes ='[';
-            let i=0;
-            while(i++ < 5) {
-                colorPalettes+=JSON.stringify(randomSplice(data))+',\n';
+http.createServer((req, res) => {
+    if (req.url != "/favicon.ico") { 
+        let colorPalettes = [];
+        let randomColorPaletteFile="./randomized_color_ palette.json";
+        writeFile(randomColorPaletteFile,colorPalettes);
+        readingFile(colorPaletteFile).then(
+            (data) => {    
+                if(data!=="Data not sufficient"){
+                   
+                    // selecting random color palette from json file
+                    let i=0;
+                    while(i++ < 5) {
+                        colorPalettes.push(randomSplice(data));
+                    }
+                    writeFile(randomColorPaletteFile, colorPalettes);
+                    readingFile(randomColorPaletteFile).then(
+                        (data1)=>{
+                            res.write(JSON.stringify(data1));
+                            res.end();
+                        }
+                    ); 
+                } 
+                else{
+                    res.write("Data not sufficient");
+                    res.end();
+                }
             }
-            // selecting random color palette from json file
-            colorPalettes+=']';
-            fs.writeFileSync("./randomized_color_ palette.json", colorPalettes);
-            fs.readFile("./randomized_color_ palette.json","utf-8",(err,data)=>{
-                res.write(data);
-                res.end();
-            })
-        });
-    } 
+        )
+    }
 }).listen(4000);
+
 
